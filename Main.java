890e5107b1cbcb89429c2bea46c5c42b58931517
Rishabh Raj <rishabhraj240405@gmail.com>
2024-05-07 22:10:50 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.io.PrintStream;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Systemmmmmmm");
+        System.out.print("Systemm");
+        PrintStream systemmmmmmmmmmmm = System.out.printf("systemmmmmmmmmmmm");
+    }
+}
